6a31bd286c81e9d3f119f4b750e1dc97d680ac63
Dark_Zelda92 <Dark_Zelda92@DESKTOP-747V88I>
2022-10-28 06:40:10 -0700

    Add project files.

---
+++ daTimer/streamTimer.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace daTimer
+{
+    partial class StreamTimer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.zeldaBTN = new System.Windows.Forms.Button();
+            this.splatoonBTN = new System.Windows.Forms.Button();
+            this.loadBTN = new System.Windows.Forms.Button();
+            this.saveBTN = new System.Windows.Forms.Button();
+            this.startBTN = new System.Windows.Forms.Button();
+            this.stopBTN = new System.Windows.Forms.Button();
+            this.resetBTN = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ZeldaTimer = new System.Windows.Forms.Label();
+            this.zeldaTime = new System.Windows.Forms.Label();
+            this.splatoonTimer = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // zeldaBTN
+            // 
+            this.zeldaBTN.Font = new System.Drawing.Font("Triforce", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.zeldaBTN.Location = new System.Drawing.Point(12, 226);
+            this.zeldaBTN.Name = "zeldaBTN";
+            this.zeldaBTN.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.zeldaBTN.Size = new System.Drawing.Size(75, 23);
+            this.zeldaBTN.TabIndex = 0;
+            this.zeldaBTN.Text = "Zedal";
+            this.zeldaBTN.UseVisualStyleBackColor = true;
+            this.zeldaBTN.Click += new System.EventHandler(this.zeldaBTN_Click);
+            // 
+            // splatoonBTN
+            // 
+            this.splatoonBTN.Font = new System.Drawing.Font("Splatfont 2", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.splatoonBTN.Location = new System.Drawing.Point(93, 226);
+            this.splatoonBTN.Name = "splatoonBTN";
+            this.splatoonBTN.Size = new System.Drawing.Size(75, 23);
+            this.splatoonBTN.TabIndex = 1;
+            this.splatoonBTN.Text = "Splatoon";
+            this.splatoonBTN.UseVisualStyleBackColor = true;
+            this.splatoonBTN.Click += new System.EventHandler(this.splatoonBTN_Click);
+            // 
+            // loadBTN
+            // 
+            this.loadBTN.Location = new System.Drawing.Point(405, 226);
+            this.loadBTN.Name = "loadBTN";
+            this.loadBTN.Size = new System.Drawing.Size(75, 23);
+            this.loadBTN.TabIndex = 3;
+            this.loadBTN.Text = "Load";
+            this.loadBTN.UseVisualStyleBackColor = true;
+            // 
+            // saveBTN
+            // 
+            this.saveBTN.Location = new System.Drawing.Point(324, 226);
+            this.saveBTN.Name = "saveBTN";
+            this.saveBTN.Size = new System.Drawing.Size(75, 23);
+            this.saveBTN.TabIndex = 4;
+            this.saveBTN.Text = "Save";
+            this.saveBTN.UseVisualStyleBackColor = true;
+            // 
+            // startBTN
+            // 
+            this.startBTN.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.startBTN.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.startBTN.Location = new System.Drawing.Point(134, 167);
+            this.startBTN.Name = "startBTN";
+            this.startBTN.Size = new System.Drawing.Size(75, 23);
+            this.startBTN.TabIndex = 5;
+            this.startBTN.Text = "Start";
+            this.startBTN.UseVisualStyleBackColor = false;
+            this.startBTN.Click += new System.EventHandler(this.startBTN_Click_1);
+            // 
+            // stopBTN
+            // 
+            this.stopBTN.Location = new System.Drawing.Point(214, 167);
+            this.stopBTN.Name = "stopBTN";
+            this.stopBTN.Size = new System.Drawing.Size(75, 23);
+            this.stopBTN.TabIndex = 6;
+            this.stopBTN.Text = "Stop";
+            this.stopBTN.UseVisualStyleBackColor = true;
+            this.stopBTN.Click += new System.EventHandler(this.stopBTN_Click);
+            // 
+            // resetBTN
+            // 
+            this.resetBTN.Location = new System.Drawing.Point(295, 167);
+            this.resetBTN.Name = "resetBTN";
+            this.resetBTN.Size = new System.Drawing.Size(75, 23);
+            this.resetBTN.TabIndex = 7;
+            this.resetBTN.Text = "Reset";
+            this.resetBTN.UseVisualStyleBackColor = true;
+            this.resetBTN.Click += new System.EventHandler(this.resetBTN_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 199);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 22);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Fonts";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(320, 199);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 22);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Save";
+            // 
+            // ZeldaTimer
+            // 
+            this.ZeldaTimer.AutoSize = true;
+            this.ZeldaTimer.Font = new System.Drawing.Font("Triforce", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ZeldaTimer.ForeColor = System.Drawing.SystemColors.Control;
+            this.ZeldaTimer.Location = new System.Drawing.Point(56, 48);
+            this.ZeldaTimer.Name = "ZeldaTimer";
+            this.ZeldaTimer.Size = new System.Drawing.Size(381, 69);
+            this.ZeldaTimer.TabIndex = 10;
+            this.ZeldaTimer.Text = "00:00:00:00";
+            // 
+            // zeldaTime
+            // 
+            this.zeldaTime.AutoSize = true;
+            this.zeldaTime.Font = new System.Drawing.Font("Triforce", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.zeldaTime.ForeColor = System.Drawing.SystemColors.Control;
+            this.zeldaTime.Location = new System.Drawing.Point(56, 48);
+            this.zeldaTime.Name = "zeldaTime";
+            this.zeldaTime.Size = new System.Drawing.Size(381, 69);
+            this.zeldaTime.TabIndex = 11;
+            this.zeldaTime.Text = "00:00:00:00";
+            // 
+            // splatoonTimer
+            // 
+            this.splatoonTimer.AutoSize = true;
+            this.splatoonTimer.Font = new System.Drawing.Font("Splatfont 2", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.splatoonTimer.ForeColor = System.Drawing.SystemColors.Control;
+            this.splatoonTimer.Location = new System.Drawing.Point(42, 24);
+            this.splatoonTimer.Name = "splatoonTimer";
+            this.splatoonTimer.Size = new System.Drawing.Size(419, 94);
+            this.splatoonTimer.TabIndex = 12;
+            this.splatoonTimer.Text = "00:00:00:00";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 8);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 14;
+            // 
+            // StreamTimer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(177)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(509, 261);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.splatoonTimer);
+            this.Controls.Add(this.zeldaTime);
+            this.Controls.Add(this.ZeldaTimer);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.resetBTN);
+            this.Controls.Add(this.stopBTN);
+            this.Controls.Add(this.startBTN);
+            this.Controls.Add(this.saveBTN);
+            this.Controls.Add(this.loadBTN);
+            this.Controls.Add(this.splatoonBTN);
+            this.Controls.Add(this.zeldaBTN);
+            this.Name = "StreamTimer";
+            this.Text = "Stream Timer";
+            this.Load += new System.EventHandler(this.StreamTimer_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button zeldaBTN;
+        private System.Windows.Forms.Button splatoonBTN;
+        private System.Windows.Forms.Button loadBTN;
+        private System.Windows.Forms.Button saveBTN;
+        private System.Windows.Forms.Button startBTN;
+        private System.Windows.Forms.Button stopBTN;
+        private System.Windows.Forms.Button resetBTN;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label ZeldaTimer;
+        private System.Windows.Forms.Label zeldaTime;
+        private System.Windows.Forms.Label splatoonTimer;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ daTimer/streamTimer.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace daTimer
+{
+ 
+    public partial class StreamTimer : Form
+    {
+        System.Timers.Timer timer;
+        int h, m, s, ms;
+
+
+        private void startBTN_Click_1(object sender, EventArgs e)
+        {
+            timer.Start();
+        }
+
+        private void stopBTN_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+        }
+
+        private void resetBTN_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+            h = 0;
+            m = 0;
+            s = 0;
+            ms = 0;
+            zeldaTime.Text = "00:00:00:00";
+            splatoonTimer.Text = "00:00:00:00";
+            
+        }
+
+        private void zeldaBTN_Click(object sender, EventArgs e)
+        {
+            zeldaTime.Visible = true;
+            splatoonTimer.Visible = false;
+            
+        }
+
+        private void splatoonBTN_Click(object sender, EventArgs e)
+        {
+            zeldaTime.Visible = false;
+            splatoonTimer.Visible = true;
+            
+        }
+
+
+        private void StreamTimer_Load_1(object sender, EventArgs e)
+        {
+            timer = new System.Timers.Timer();
+            timer.Interval = 1;
+            timer.Elapsed += OnTimeEvent;
+        }
+
+        public StreamTimer()
+        {
+            InitializeComponent();
+        } 
+
+
+        private void OnTimeEvent(object sender, ElapsedEventArgs e)
+        {
+            Invoke(new Action(() => {
+                ms += 1;
+                if (ms == 100)
+                {
+                    ms = 0;
+                    s += 1;
+                }
+                if (s == 60)
+                {
+                    s = 0;
+                    m += 1;
+                }
+                if (m == 60)
+                {
+                    m = 0;
+                    h += 1;
+                }
+                zeldaTime.Text = string.Format("{0}:{1}:{2}:{3}", h.ToString().ToString().PadLeft(2,'0'), m.ToString().ToString().PadLeft(2, '0'), s.ToString().ToString().PadLeft(2, '0'), ms.ToString().ToString().PadLeft(2, '0'));
+                splatoonTimer.Text = string.Format("{0}:{1}:{2}:{3}", h.ToString().ToString().PadLeft(2, '0'), m.ToString().ToString().PadLeft(2, '0'), s.ToString().ToString().PadLeft(2, '0'), ms.ToString().ToString().PadLeft(2, '0'));
+               
+            }));
+        }
+    }
+}
